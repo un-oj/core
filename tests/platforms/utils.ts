bde3a7c75b5449bdf818257dda0d424c926b8036
Typed SIGTERM <typed.sigterm@gmail.com>
2025-05-07 02:32:19 +0800

    test: fix

--- tests/platforms/utils.ts
+++ tests/platforms/utils.ts
@@ -1,9 +1,9 @@
-import type { Problem } from "@un-oj/core";
+import type { Problem } from '@un-oj/core';
 import { expect } from 'bun:test';
 
 export function assertProblem(problem: Problem, expected?: Partial<{
-  type: string;
-}>) {
+  type: string
+}>): void {
   if (expected?.type)
     expect(problem.type).toBe(expected.type);
   expect(problem).toMatchSnapshot();
